dafa9d1e2677b5becf2d3449958b3627dfca2a62
Adam Young <adamyoungy678@gmail.com>
2023-07-16 19:11:22 +0100

    chore: environment setup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,16 +6,13 @@ const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+        <IonToolbar color="secondary">
+          <IonTitle>FreeCodeCamp LIVE</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+        
+        Hello World
         <ExploreContainer />
       </IonContent>
     </IonPage>
