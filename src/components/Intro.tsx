2ce4ba2cb2f132d275cc1141bd33823e12b7801e
Adam Young <adamyoungy678@gmail.com>
2023-07-17 13:56:06 +0100

    feat: added a swiper button next to take you to the next slide

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,6 +1,6 @@
-import { IonContent, IonHeader, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
-import { Swiper, SwiperSlide} from 'swiper/react';
+import { Swiper, SwiperSlide, useSwiper} from 'swiper/react';
 import 'swiper/css'
 import './Intro.css'
 import Intro1Svg from '../assets/intro/1.svg';
@@ -9,6 +9,11 @@ import Intro3Svg from '../assets/intro/3.svg';
 
 interface ContainerProps { 
   onFinish: () => void;
+}
+
+const SwiperButtonNext = ({ children }: any) => {
+  const swiper = useSwiper();
+  return <IonButton onClick={() => swiper.slideNext()}>{children}</IonButton>;
 }
 
 const Intro: React.FC<ContainerProps> = () => {
@@ -20,6 +25,7 @@ const Intro: React.FC<ContainerProps> = () => {
         <IonText>
           <h3>Build awesome apps with Ionic UI components!</h3>
         </IonText>
+        <SwiperButtonNext>Next</SwiperButtonNext>
       </SwiperSlide>
       <SwiperSlide>easst</SwiperSlide>
     </Swiper>
