d80a14798c9d83ac96b3b77b6bc4ac568f431cbe
Adam Young <adamyoungy678@gmail.com>
2023-07-17 09:57:31 +0100

    feat: added an icon to login button

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,6 @@
-import { IonButton, IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonIcon, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import { logInOutline } from 'ionicons/icons';
 
 const Login: React.FC = () => {
     const doLogin = (event: any) => {
@@ -21,7 +22,10 @@ const Login: React.FC = () => {
                         <form onSubmit={doLogin}>
                             <IonInput fill="outline" labelPlacement="floating" label="Email" type="email" placeholder="email@gmail.com" />
                             <IonInput className="ion-margin-top" fill="outline" labelPlacement="floating" label="Password" type="password" placeholder="email@gmail.com" />
-                            <IonButton type="submit" expand="block" className="ion-margin-top">Login</IonButton>
+                            <IonButton type="submit" expand="block" className="ion-margin-top">
+                                Login
+                                <IonIcon icon={logInOutline} slot="end"/>
+                            </IonButton>
                             <IonButton routerLink="/register" color={'secondary'} type="button" expand="block" className="ion-margin-top">Create account</IonButton>
                         </form>
                     </IonCardContent>
