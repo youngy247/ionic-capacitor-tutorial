98dc678871a530d535d7a873443a81f5e571bdbb
Adam Young <adamyoungy678@gmail.com>
2023-07-17 09:46:38 +0100

    feat: added form with email and password fields

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,31 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardContent, IonContent, IonFooter, IonHeader, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 
 const Login: React.FC = () => {
+    const doLogin = (event: any) => {
+        event.preventDefault();
+        console.log('doLogin');
+    }
+    
 
     return (
         <IonPage>
             <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Page Title</IonTitle>
+                <IonToolbar color={'primary'}>
+                    <IonTitle>Title</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent className="ion-padding">
-                UI goes here...
+            <IonContent>
+                <IonCard>
+                    <IonCardContent>
+                        <form onSubmit={doLogin}>
+                            <IonInput fill="outline" labelPlacement="floating" label="Email" type="email" placeholder="email@gmail.com" />
+                            <IonInput className="ion-margin-top" fill="outline" labelPlacement="floating" label="Password" type="password" placeholder="email@gmail.com" />
+                            </form>
+                    </IonCardContent>
+                </IonCard>
             </IonContent>
+            
         </IonPage>
     );
 };
