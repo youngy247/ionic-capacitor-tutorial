072bdbcaf04897ceb7c122afcea86172c52f1534
Adam Young <adamyoungy678@gmail.com>
2023-07-17 15:57:49 +0100

    feat: added responsive Ionic grid for registration page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,43 +1,86 @@
-import { IonBackButton, IonButton, IonButtons, IonCard, IonCardContent, IonContent, IonHeader, IonIcon, IonInput, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
-import { checkmarkDoneOutline, logInOutline, personCircleOutline } from 'ionicons/icons';
-import React from 'react';
+import {
+  IonBackButton,
+  IonButton,
+  IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonPage,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+  useIonRouter,
+} from "@ionic/react";
+import {
+  checkmarkDoneOutline,
+  logInOutline,
+  personCircleOutline,
+} from "ionicons/icons";
+import React from "react";
 
 const Register: React.FC = () => {
-    const router = useIonRouter();
-    const doRegister = (event: any) => {
-        event.preventDefault();
-        console.log('doRegister');
-        router.goBack();
-    }
+  const router = useIonRouter();
+  const doRegister = (event: any) => {
+    event.preventDefault();
+    console.log("doRegister");
+    router.goBack();
+  };
 
-    return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar color={'primary'}>
-                    <IonButtons slot="start">
-                        <IonBackButton defaultHref="/" />
-                    </IonButtons>
-                    <IonTitle>Create Account</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent scrollY={false}>
-                
-                <IonCard>
-                    <IonCardContent>
-                        <form onSubmit={doRegister}>
-                            <IonInput fill="outline" labelPlacement="floating" label="Email" type="email" placeholder="email@gmail.com" />
-                            <IonInput className="ion-margin-top" fill="outline" labelPlacement="floating" label="Password" type="password" placeholder="email@gmail.com" />
-                            <IonButton type="submit" expand="block" className="ion-margin-top">
-                                Create my account
-                                <IonIcon icon={checkmarkDoneOutline} slot="end"/>
-                            </IonButton>
-                        </form>
-                    </IonCardContent>
-                </IonCard>
-            </IonContent>
-            
-        </IonPage>
-    );
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color={"primary"}>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/" />
+          </IonButtons>
+          <IonTitle>Create Account</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent scrollY={false}>
+        <IonGrid fixed>
+          <IonRow class="ion-justify-content-center">
+            <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="4">
+
+        <IonCard>
+          <IonCardContent>
+            <form onSubmit={doRegister}>
+              <IonInput
+                fill="outline"
+                labelPlacement="floating"
+                label="Email"
+                type="email"
+                placeholder="email@gmail.com"
+              />
+              <IonInput
+                className="ion-margin-top"
+                fill="outline"
+                labelPlacement="floating"
+                label="Password"
+                type="password"
+                placeholder="email@gmail.com"
+              />
+              <IonButton
+                type="submit"
+                expand="block"
+                className="ion-margin-top"
+              >
+                Create my account
+                <IonIcon icon={checkmarkDoneOutline} slot="end" />
+              </IonButton>
+            </form>
+          </IonCardContent>
+        </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Register;
