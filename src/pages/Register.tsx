baf33f01d8cbd97abae15aa0fa8c49994d70d866
Adam Young <adamyoungy678@gmail.com>
2023-07-17 10:45:22 +0100

    feat: created register form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,18 +1,37 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonContent, IonHeader, IonIcon, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { checkmarkDoneOutline, logInOutline, personCircleOutline } from 'ionicons/icons';
 import React from 'react';
 
 const Register: React.FC = () => {
 
+    const doRegister = (event: any) => {
+        event.preventDefault();
+        console.log('doRegister');
+    }
+
     return (
         <IonPage>
             <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Page Title</IonTitle>
+                <IonToolbar color={'primary'}>
+                    <IonTitle>Create Account</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent className="ion-padding">
-                UI goes here...
+            <IonContent scrollY={false}>
+                
+                <IonCard>
+                    <IonCardContent>
+                        <form onSubmit={doRegister}>
+                            <IonInput fill="outline" labelPlacement="floating" label="Email" type="email" placeholder="email@gmail.com" />
+                            <IonInput className="ion-margin-top" fill="outline" labelPlacement="floating" label="Password" type="password" placeholder="email@gmail.com" />
+                            <IonButton type="submit" expand="block" className="ion-margin-top">
+                                Create my account
+                                <IonIcon icon={checkmarkDoneOutline} slot="end"/>
+                            </IonButton>
+                        </form>
+                    </IonCardContent>
+                </IonCard>
             </IonContent>
+            
         </IonPage>
     );
 };
