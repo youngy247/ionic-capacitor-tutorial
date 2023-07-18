5304ccb6f26155af5be81dd45a632edd23a8601b
Adam Young <adamyoungy678@gmail.com>
2023-07-18 11:54:39 +0100

    feat: added menu layout component

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,18 +1,30 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonMenu, IonPage, IonRouterOutlet, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import List from './List';
+import Settings from './Settings';
+import { Redirect, Route } from 'react-router';
 
 const Menu: React.FC = () => {
 
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Page Title</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        UI goes here...
-      </IonContent>
+      <IonMenu contentId="main">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Page Title</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+          <IonContent className="ion-padding">menu items</IonContent>
+      </IonMenu>
+
+      <IonRouterOutlet id="main">
+        <Route exact path="/app/list" component={List} />
+        <Route path="/app/settings" component={Settings} />
+        <Route exact path="/app">
+          <Redirect to="/app/list" />
+        </Route>
+      </IonRouterOutlet>
+
     </IonPage>
   );
 };
