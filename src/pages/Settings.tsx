10b4c59ba38f5a2c08f91a9ec9208fd2dd2bb142
Adam Young <adamyoungy678@gmail.com>
2023-07-18 12:45:30 +0100

    feat: adding routing for tab menu in for Settings

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,22 +1,27 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import { Redirect, Route } from 'react-router';
+import Tab1 from './Tab1';
+import Tab2 from './Tab2';
 
 const Seetings: React.FC = () => {
 
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar color={'primary'}>
-        <IonButtons slot="start">
-          <IonMenuButton />
-        </IonButtons>
-          <IonTitle>Settings</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        UI goes here...
-      </IonContent>
-    </IonPage>
+    <IonTabs>
+      <IonTabBar>
+        <IonTabButton tab="tab1" href="/app/settins/tab1"></IonTabButton>
+      </IonTabBar>
+
+     <IonRouterOutlet>
+        <Route path="/app/settings/tab1" component={Tab1} />
+        <Route path="/app/settings/tab2" component={Tab2} />
+
+        <Route exact path="/app/settings">
+          <Redirect to="/app/settings/tab1" />
+        </Route>
+     </IonRouterOutlet>
+
+    </IonTabs>
   );
 };
 
