0a67a9feaef055f7c7233d5ab9d5ae68d6d790ea
Adam Young <adamyoungy678@gmail.com>
2023-07-18 12:51:07 +0100

    feat: added tab buttons with icons labels and links to tab bar at bottom of the page

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,15 +1,23 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonIcon, IonLabel, IonMenuButton, IonPage, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 import { Redirect, Route } from 'react-router';
 import Tab1 from './Tab1';
 import Tab2 from './Tab2';
+import { ellipse, triangle } from 'ionicons/icons';
 
 const Seetings: React.FC = () => {
 
   return (
     <IonTabs>
-      <IonTabBar>
-        <IonTabButton tab="tab1" href="/app/settins/tab1"></IonTabButton>
+      <IonTabBar slot="bottom">
+        <IonTabButton tab="tab1" href="/app/settings/tab1">
+          <IonIcon icon={triangle} />
+          <IonLabel>Tab 1</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="tab2" href="/app/settings/tab2">
+          <IonIcon icon={ellipse} />
+          <IonLabel>Tab 2</IonLabel>
+        </IonTabButton>
       </IonTabBar>
 
      <IonRouterOutlet>
