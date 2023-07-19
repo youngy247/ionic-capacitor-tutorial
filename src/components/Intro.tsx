1b0d53f35f37ec2b878bc35451922de15fd52fa1
Adam Young <adamyoungy678@gmail.com>
2023-07-19 18:40:04 +0100

    feat: COMPLETE TUTORIAL and updated tutorial pages content for new project

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -22,24 +22,43 @@ const Intro: React.FC<ContainerProps> = ({ onFinish }) => {
     <Swiper>
       <SwiperSlide>
         <img src={Intro1Svg} alt="intro1" />
-        <IonText>
-          <h3>Build awesome apps with Ionic UI components!</h3>
+        <IonText className="intro-text">
+          <h2>Insect Identifier and Tracker</h2>
+          <p>Welcome to the Insect Identifier and Tracker. Become a citizen scientist and contribute to the understanding of insect biodiversity!</p>
         </IonText>
         <SwiperButtonNext>Next</SwiperButtonNext>
       </SwiperSlide>
 
       <SwiperSlide>
         <img src={Intro2Svg} alt="intro2" />
-        <IonText>
-          <h3>Create powerful native apps with Capacitor.</h3>
+        <IonText className="intro-text">
+          <h2>Identify and Record Sightings</h2>
+          <p>Input descriptive details to identify insects. Record your sightings with date, location, and a photo. Every observation matters!</p>
         </IonText>
         <SwiperButtonNext>Next</SwiperButtonNext>
       </SwiperSlide>
 
       <SwiperSlide>
         <img src={Intro3Svg} alt="intro3" />
-        <IonText>
-          <h3>Enjoy learning to code!</h3>
+        <IonText className="intro-text">
+          <h2>Sharing Data and Challenges</h2>
+          <p>Your data is shared with conservation projects. Engage in fun challenges like spotting a certain number of species to contribute more!</p>
+        </IonText>
+        <SwiperButtonNext>Next</SwiperButtonNext>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={Intro3Svg} alt="intro3" />
+        <IonText className="intro-text">
+          <h2>Always Ready, Anywhere</h2>
+          <p>No internet? No problem. Save your sightings offline and upload when ready. Plus, we&apos;re available on iOS, Android, and web.</p>
+        </IonText>
+        <SwiperButtonNext>Next</SwiperButtonNext>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={Intro3Svg} alt="intro3" />
+        <IonText className="intro-text">
+          <h2>Ready to Begin?</h2>
+          <p>You&apos;re all set to contribute to insect conservation. Let&apos;s start identifying and tracking!</p>
         </IonText>
         <IonButton onClick={() => onFinish()}>Finish</IonButton>
       </SwiperSlide>
